e4a34c9b9eacb34c18d8a0264467158b4a1af80b
Shivam <singhshivamkr@gmail.com>
2022-02-20 18:15:29 +0530

    :memo: Add comments on demo example

--- demo/App.tsx
+++ demo/App.tsx
@@ -3,6 +3,7 @@ import { faker } from "@faker-js/faker";
 import "./app.css";
 import Accordion from "../src";
 
+// Your list - array of objects, id is required
 const data = Array.from({ length: 200 }, () => {
   return {
     id: faker.datatype.uuid(),
@@ -11,25 +12,34 @@ const data = Array.from({ length: 200 }, () => {
   };
 });
 
+// create type if you need intellisense
 type CompProps = typeof data[0] & {
   isOpen: boolean;
+  onClick: (txt: string) => void;
 };
 
-const HeaderComp = ({ title, isOpen }: CompProps) => (
+// all the props get passed here with isOpen
+const SummaryComponent = ({ title, isOpen }: CompProps) => (
   <div className="header">
     {title} <span className={(isOpen ? "open" : "") + " chevron"}>👇</span>
   </div>
 );
 
-const ContentComp = ({ content }: CompProps) => <p>{content}</p>;
+// component will get wrapped in <div class="acc-content">
+const DetailComponent = ({ content, onClick }: CompProps) => (
+  <p onClick={() => onClick(content)}>{content}</p>
+);
 
 const App = () => {
   return (
     <div>
       <Accordion
         items={data}
-        SummaryComponent={HeaderComp}
-        DetailComponent={ContentComp}
+        // you can pass any props,
+        // it will be passed to the Detail & Summary
+        onClick={(txt: string) => alert("You clicked on\n" + txt)}
+        SummaryComponent={SummaryComponent}
+        DetailComponent={DetailComponent}
       />
     </div>
   );
